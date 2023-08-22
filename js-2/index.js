0f111f07f145db78d8c4afc394ae4428358ebdba
st-khanhngo <khanh.ngo@supremetech.vn>
2023-08-22 15:27:20 +0700

    complete js-2

--- js-2/index.js
+++ js-2/index.js
@@ -19,16 +19,32 @@
 
 
 const mergeArray = (arr1, arr2) => {
-  arr1.reduce()
+  const result = [];
+  // get unique id in arrays
+  const arr1Id = arr1.map(item => item.id).filter((value, index, arr) => arr.indexOf(value) === index);
+  const arr2Id = arr2.map(item => item.id).filter((value, index, arr) => arr.indexOf(value) === index);
+  const duplicateId = arr1Id.filter(i => arr2Id.includes(i));
+  // array with duplicated id
+  const newArr = [...arr1, ...arr2].filter(item => duplicateId.includes(item.id));
+  newArr.forEach(item => {
+    const arrItem = result.find(a => a.id === item.id);
+    if (!arrItem) {
+      result.push(item);
+    } else {
+      arrItem.quantity += item.quantity;
+    }
+  })
+  return result;
 }
 
 console.log(mergeArray([
   { id: '1', quantity: 2 },
   { id: '2', quantity: 2 },
   { id: '3', quantity: 3 },
-  { id: '1', quantity: 3 }
-],
+  { id: '1', quantity: 3 }],
   [
     { id: '1', quantity: 2 },
-    { id: '3', quantity: 2 }
-  ]));
+    { id: '3', quantity: 2 },
+    { id: '4', quantity: 2 }
+  ]
+));
